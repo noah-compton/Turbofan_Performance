53636ce2644fac9a9890e65f19d0a749ebb6afec
Noah Compton <noah.compton@ufl.edu>
2022-12-04 01:21:41 -0500

    initialization of turbofan file

---
+++ Turbofan.py
@@ -0,0 +1,5 @@
+# Date    | Description of Changes:                          | Author
+# ----------------------------------------------------------------------------
+# Dec 3   | Creation of file. This file will compile all of  |
+#         | the completed calsses.                           |
+# ----------------------------------------------------------------------------
