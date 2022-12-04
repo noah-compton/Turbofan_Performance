d977e24b9b033754c8107bb6636ee43d9bd35e8b
Noah Compton <noah.compton@ufl.edu>
2022-12-04 17:35:29 -0500

    changed name "componets" to "components". Main

---
+++ components.py
@@ -0,0 +1,21 @@
+# Initial
+# Jose R.
+
+# Import Major Components
+from inlet import Inlet  # Added, Noah C. Dec 3
+from fan import Fan  # Added, Noah C. Dec 3
+from compressor import Compressor
+from burner import Burner  # Added, Noah C. Dec 3
+from turbine import Turbine  # Added, Noah C. Dec 3
+from mixer import Mixer  # Added, Noah C. Dec 3
+from nozzle import Nozzle  # Added, Noah C. Dec 3
+
+# Define Other Components
+class Shaft:
+    def __init__(self) -> None:
+        pass
+
+
+class Duct:
+    def __init__(self) -> None:
+        pass
