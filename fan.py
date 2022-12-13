345df457044b17e006a73cf47e412acca0f83894
ihusted <118385738+ihusted@users.noreply.github.com>
2022-12-12 20:20:20 -0500

    Update fan.py
    Added second outlet pressure, mass flow, BPR

--- fan.py
+++ fan.py
@@ -20,10 +20,13 @@ class Fan:
         self.Tt_in = init.copy()
         self.W_in = init.copy()
         #Outlet
-        self.Pt_out = init.copy()
+        self.Pt_out1 = init.copy()
+        self.Pt_out2 = init.copy()
         self.Tt_out = init.copy()
-        self.W_out = init.copy()
+        self.W_out1 = init.copy()
+        self.W_out2 = init.copy()
         #Characteristics
+        self.BPR = init.copy()
         self.TR = init.copy()    # TR
         self.PR = init.copy()   # PR
         self.eff_poly = init.copy()     # eff_poly
@@ -93,7 +96,16 @@ class Fan:
                  elif len(values) < 2:
                         self.eff_poly["units"] = ""
                         raise Warning("Not enough inputs: assuming dimensionless parameter")
+                        
+            elif property == "BPR":
+                 self.BPR["value"] = value
 
+                 if len(values) == 2:
+                    self.BPR["units"] = units
+
+                 elif len(values) < 2:
+                        self.BPR["units"] = ""
+                        raise Warning("Not enough inputs: assuming dimensionless parameter")
            
             elif property == "name":
                 self.name = values
@@ -106,15 +118,21 @@ class Fan:
         
         #values
         self.TR["value"] = (self.PR["value"] ** ((y - 1)/(y*self.eff_poly["value"])))
-        self.Pt_out["value"] = self.PR["value"] * self.Pt_in["value"]
+        self.Pt_out1["value"] = self.PR["value"] * self.Pt_in["value"]
+        self.Pt_out2["value"] = self.PR["value"] * self.Pt_in["value"]
         self.Tt_out["value"] = self.TR["value"] * self.Tt_in["value"]
-        self.W_out["value"] = self.W_in["value"]
+        self.W_out1["value"] = self.W_in["value"]
+        self.W_out2["value"] = 0
+
 
         #units
         self.TR["units"] =''
-        self.Pt_out["units"] = 'Pa'
+        self.Pt_out1["units"] = 'Pa'
+        self.Pt_out2["units"] = 'Pa'
         self.Tt_out["units"] = 'K'
-        self.W_out["units"] = self.W_out["units"]
+        self.W_out1["units"] = 'kg/s'
+        self.W_out2["units"] = 'kg/s'
+
 
     def __str__(self):
         str = f"{self.name} Characteristics:\n"
